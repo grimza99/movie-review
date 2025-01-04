6ba23fd9af1031f2e978047a2fb00c29d2493984
nerte <grimza@nerteui-MacBookPro.local>
2025-01-04 20:51:15 +0900

    [Feat] add useEffect useEffect를 사용하여  최신순, 혹은 베스트 순을 누르면 서버에서 정렬해서 데이터를 받아오게 기능 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import ReviewList from "./ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getReviews from "../api";
 
 function App() {
@@ -17,9 +17,11 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
+  useEffect(() => {
+    handleLoad(order);
+  }, [order]);
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery);
     setItems(reviews);
   };
   return (
@@ -29,7 +31,6 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
